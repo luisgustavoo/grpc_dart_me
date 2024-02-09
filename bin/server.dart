915ff9a98f6977b3d759ecd3fce5e9d4e7e5cd31
Luis Gustavo <luisgustavodevbr@gmail.com>
2024-02-09 12:20:10 -0300

    Change local generated folder

--- bin/server.dart
+++ bin/server.dart
@@ -5,8 +5,12 @@ import 'package:grpc_dart_me/services/user_services.dart';
 void main(List<String> arguments) async {
   final server = Server.create(
     services: [SumServices(), UserServices()],
-    codecRegistry: CodecRegistry(codecs: const [GzipCodec(), IdentityCodec()]),
+    codecRegistry: CodecRegistry(codecs: const [
+      GzipCodec(),
+      // IdentityCodec(),
+    ]),
   );
+
   await server.serve(port: 50051);
   print('Server listening on port ${server.port}...');
 }
---
+++ lib/src/grpc_dart_me.dart
@@ -0,0 +1,4 @@
+export '../generated/user/user.pb.dart';
+// export 'generated/user/user.pbenum.dart';
+export '../generated/user/user.pbgrpc.dart';
+export '../generated/user/user.pbjson.dart';
